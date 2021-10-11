f5703558a02cc0bfa7e0fcbb493e0c8084bfdcaa
Marcelo Neri da Silva <marceloneri@gmail.com>
2021-10-11 01:27:18 -0300

    update

---
+++ src/ui/components/data-display/PageTitle/PageTitle.tsx
@@ -0,0 +1,18 @@
+import React from 'react';4
+import { PageTitleContainer } from './PageTitle.style';
+
+interface PageTitleProps { 
+    title: string;
+    subtitle: string;
+}
+
+const PageTitle: React.FC<PageTitleProps> = (props) => {
+    return(
+        <PageTitleContainer>
+            {props.title}
+            {props.subtitle}
+        </PageTitleContainer>
+    );
+};
+
+export default PageTitle;
---
+++ src/ui/components/feedback/SafeEnvironment/SafeEnvironment.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SaveEnvironmentContainer } from './SafeEnvironment.slyle';
+import { Container } from '@mui/material';
+
+const  SafeEnvironment = () => {
+    return (
+        <SaveEnvironmentContainer>
+            <Container>  
+                Ambiente seguro              
+            </Container>
+        </SaveEnvironmentContainer>
+    );
+};
+
+export default SafeEnvironment;
